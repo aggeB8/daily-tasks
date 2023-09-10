9ed4a6c625ea7dd5206ebc275dd87a82c4b17cd7
aggeB8 <august.brannmark@icloud.com>
2023-09-10 08:52:41 +0200

    +localstorage start

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-
+  const tasks = localStorage.getItem('tasks')
 </script>
 
 <template>
-
+  <table v-if="tasks">
+    <tr v-for="task in tasks"></tr>
+  </table>
+  <h1 v-else>no</h1>
 </template>
 
 <style scoped>
